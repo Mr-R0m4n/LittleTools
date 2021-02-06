2be9ce21e8a323ee4fd110f32ff594a1ba23eaad
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-02-06 11:02:39 +0100

    Update ceasar-verschluesselung.py

--- verschluesselungen/ceasar-verschluesselung.py
+++ verschluesselungen/ceasar-verschluesselung.py
@@ -4,16 +4,25 @@ Cäsar-Verschlüsselung mit verschiebung um Zeichen Index
 
 
 # Funktion zur Cäsar-Verschlüsselung
-def caesar(kette):
-    print("Cäsar-Verschlüsselung")
+def caesar(zeichenkette):
+    print("Cäsar-Verschlüsselung um Zeichen Index")
+    # Eingabe des Verschiebewertes
+    verschiebung = int(input("Bitte geben Sie den Verschiebewert ein: "))
+    print("\n")
+    print("Codiert: ", end="")
     # Index zur Erhöhung des Verschiebewertes
     index = 1
-    # Jedes Zeichen um den jeweiligen Index verschieben
-    for zeichen in kette:
+    # Jedes Zeichen um den Verschiebewert und jeweiligen Index in der Zeichenkette verschieben
+    for zeichen in zeichenkette:
         # Falls es ein Großbuchstabe ist
         if zeichen.isupper():
-            print(chr((ord(zeichen) + len(kette[:index]) - 65) % 26 + 65), end=" ")
+            print(chr((ord(zeichen) + (verschiebung + len(zeichenkette[:index])) - 65) % 26 + 65), end=" ")
         else:
-            print(chr((ord(zeichen) + len(kette[:index]) - 97) % 26 + 97), end=" ")
+            print(chr((ord(zeichen) + (verschiebung + len(zeichenkette[:index])) - 97) % 26 + 97), end=" ")
         # Erhöhung des Verschiebewertes
         index += 1
+
+
+eingabe = input("Bitte geben Sie die zu verschlüsselnde Zeichenkette ein: ")
+print("\n")
+caesar(eingabe)
